a768e3325729537ecc77dd16bc8f2b222f584230
kok-s0s <2694308562@qq.com>
2022-01-06 15:44:45 +0800

    修改NoMatch-404样式 v1.5

--- src/view/NoMatch/NoMatch.js
+++ src/view/NoMatch/NoMatch.js
@@ -3,7 +3,7 @@ import './NoMatch.scss';
 const NoMatch = () => {
     return (
         <div className="no__match">
-            <span>抱歉没有你想要找到的内容～～～</span>
+            <span>抱歉没有你想要找到的内容～</span>
             <img src="assets/pity.png" alt="pity" />
         </div>
     );
