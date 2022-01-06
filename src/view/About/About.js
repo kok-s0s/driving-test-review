6dbead84ed61c8b5cbf31879997de1f53c727475
kok-s0s <2694308562@qq.com>
2022-01-06 13:25:55 +0800

    关于页面 v1.2

--- src/view/About/About.js
+++ src/view/About/About.js
@@ -1,7 +1,27 @@
 import './About.scss';
 
 const About = () => {
-    return <div className="about">About</div>;
+    return (
+        <div className="about">
+            <div className="personal__info">
+                <p>00后</p>
+                <p>折腾“怪”</p>
+                <p>脑子想法很多</p>
+                <p>不讨厌敲代码</p>
+                <p>喜欢冲浪吃瓜</p>
+                <p>
+                    <a href="https://github.com/kok-s0s" alt="My Github">
+                        Github README
+                    </a>
+                </p>
+                <p>
+                    <a href="https://kok-s0s.top/" alt="Blog">
+                        My Blog
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default About;
