5bef03d46ee398d36cd3d305fcecd1813581f052
kok-s0s <2694308562@qq.com>
2022-01-12 14:13:18 +0800

    资料库页面初步制作 v2.1

--- src/views/DataBase/DataBase.js
+++ src/views/DataBase/DataBase.js
@@ -1,7 +1,43 @@
 import './DataBase.scss';
+import React, { useState } from 'react';
+// import Aanalysis from '../../components/Aanalysis/Aanalysis';
+import Cognition from '../../components/Cognition/Cognition';
+import Graphics from '../../components/Graphics/Graphics';
+import Button from '../../components/Button/Button';
 
 const DataBase = () => {
-    return <div className="database">DataBase</div>;
+    const [currentStatus, setCurrentStatus] = useState(1);
+
+    return (
+        <div className="database">
+            <div className="database__nav">
+                <Button
+                    name="常识"
+                    style={{ margin: '0.2rem', width: '25%' }}
+                    onClick={() => setCurrentStatus(1)}
+                />
+                <Button
+                    name="图标"
+                    style={{ margin: '0.2rem', width: '25%' }}
+                    onClick={() => setCurrentStatus(2)}
+                />
+            </div>
+
+            <div
+                className={`${
+                    currentStatus === 1 ? '' : 'hide'
+                } show__cognition`}>
+                <Cognition />
+            </div>
+            <div
+                className={`${
+                    currentStatus === 2 ? '' : 'hide'
+                } show__graphics`}>
+                <Graphics />
+            </div>
+            {/* <Aanalysis /> */}
+        </div>
+    );
 };
 
 export default DataBase;
